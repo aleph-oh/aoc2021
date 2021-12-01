2513915593b7bafe14c758d146b2ffd78bcbc6e6
aleph-oh <jhilton@mit.edu>
2021-12-01 03:06:14 -0500

    Change main to accept CLI arg for which day

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod day1;
 mod utils;
 
-const DAY: usize = 1;
-
 fn main() {
-    if DAY == 1 {
-        let (pt1, pt2) = day1::solve();
-        println!("day 1 part 1: {} part 2: {}", pt1, pt2);
-    }
+    let day = std::env::args()
+        .skip(1)
+        .next()
+        .expect("Expected one argument for day")
+        .parse::<u8>()
+        .expect("Could not parse provided day as u8");
+    let (pt1, pt2) = match day {
+        1 => day1::solve(),
+        _ => panic!(),
+    };
+    println!("Day {} | Part 1 {} | Part 2 {}", day, pt1, pt2);
 }
